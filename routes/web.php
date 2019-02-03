8d62f7e03f4f7adcb0c138328242445680f3e945
Rufus Maiwald <rufi@mcone.eu>
2019-02-03 16:46:19 +0100

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+$router->get('/version', function () use ($router) {
+    return $router->app->version();
+});
+
+/**
+ * Always the last route! Loads Vue Application.
+ */
+$router->get('/{route:.*}/', function () {
+    return view('app');
+});
